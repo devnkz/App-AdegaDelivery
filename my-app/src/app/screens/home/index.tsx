7a6b3b586a7ed27106a9bf7df61d9ea5c90dbb17
devnkz <vieiranyckolas1@gmail.com>
2024-10-01 19:11:44 -0300

    att area searchBar

--- my-app/src/app/screens/home/index.tsx
+++ my-app/src/app/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, ScrollView, Pressable, View, Button, FlatList } from 'react-native';
+import { Text, ScrollView, Pressable, View, Button, FlatList, Image } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
@@ -77,10 +77,29 @@ const Home = () => {
                             <FlatList
                                 data={results}
                                 scrollEnabled={false}
-                                keyExtractor={(item) => item.id.toString()} 
+                                keyExtractor={(item) => item.id.toString()}
                                 renderItem={({ item }) => (
-                                    <View style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: 'gray' }}>
-                                        <Text>{item.nome} - R${item.preco}</Text>
+                                    <View className='w-full items-center mt-4'>
+                                        <View className='w-11/12 flex flex-row justify-between bg-white p-3 rounded-lg items-center' style={{ elevation: 10 }}>
+                                            <View>
+                                                <Text className='text-3xl font-bold'>{item.nome}</Text>
+                                                <Text>{item.tipo}</Text>
+                                                <Text>{item.modelo}</Text>
+                                                <Text>{item.tamanho}</Text>
+                                                <Text className='text-3xl font-light text-green-600'>R$ {item.preco}</Text>
+
+                                                <Pressable onPress={() => alert('penis')} style={{ elevation: 5 }} className='flex flex-row items-center gap-2 w-36 justify-center rounded-lg p-2 bg-black mt-4'>
+                                                    <Feather name={'shopping-bag'} size={24} color={'white'} />
+                                                    <Text className='text-base font-semibold text-white '>Adicionar</Text>
+                                                </Pressable>
+                                            </View>
+                                            <View>
+                                                <Image
+                                                    source={{ uri: item.url_image }}
+                                                    className='h-52 w-24'
+                                                />
+                                            </View>
+                                        </View>
                                     </View>
                                 )}
                             />
