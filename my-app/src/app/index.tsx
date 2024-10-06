b1ee777cceb61000514f59522c78987240078657
devnkz <vieiranyckolas1@gmail.com>
2024-10-06 12:45:45 -0300

    .

--- my-app/src/app/index.tsx
+++ my-app/src/app/index.tsx
@@ -1,7 +1,5 @@
 import 'react-native-reanimated';
 import 'react-native-gesture-handler';
-import { BagProvider } from './components/contextBag';
-import Home from './screens/home';
 import Login from './screens/login';
 
 export default function App(){
